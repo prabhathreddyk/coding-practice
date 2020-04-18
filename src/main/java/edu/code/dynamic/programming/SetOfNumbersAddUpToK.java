aff15bd0077aa587b9c00a690148610f1ea58e1a
prabhathkatta <prabhathreddy29@gmail.com>
2020-04-18 11:40:18 -0500

    DP

--- src/main/java/edu/code/dynamic/programming/SetOfNumbersAddUpToK.java
+++ src/main/java/edu/code/dynamic/programming/SetOfNumbersAddUpToK.java
@@ -5,10 +5,17 @@ import java.util.Arrays;
 public class SetOfNumbersAddUpToK {
 
 	public static void main(String[] args) {
-		int[] array = {2,4,6,10,3,1};
+		int[] array = {7,2,4,6,10,3,1};
 		System.out.println("Number of subsets: "+findSetOfNumbersAddToK(array, 6));
+		System.out.println("Number of subsets: "+findSetOfNumbersAddToK(array, 6, 0));
 	}
 
+	/**
+	 * Easy to understand sub array but creates more sub arrays
+	 * @param array
+	 * @param k
+	 * @return
+	 */
 	private static int findSetOfNumbersAddToK(int[] array, int k){
 //		System.out.println(Arrays.toString(array) + " , k="+k);
 		if ( k == 0){
@@ -22,6 +29,29 @@ public class SetOfNumbersAddUpToK {
 			count+=findSetOfNumbersAddToK(Arrays.copyOfRange(array, 1, array.length), k-array[0]);
 		}
 		count+=findSetOfNumbersAddToK(Arrays.copyOfRange(array, 1, array.length), k);
+		return count;
+	}
+	
+	/**
+	 * Efficient than above function 
+	 * @param array
+	 * @param k
+	 * @param index
+	 * @return
+	 */
+	private static int findSetOfNumbersAddToK(int[] array, int k, int index){
+//		System.out.println(Arrays.toString(array) + " , k="+k + ", index="+index);
+		if ( k == 0){
+			return 1;
+		}
+		if (index > array.length-1){
+			return 0;
+		}
+		int count = 0;
+		if (k-array[index] >= 0){
+			count+=findSetOfNumbersAddToK(array, k-array[index], index+1);
+		}
+		count+=findSetOfNumbersAddToK(array, k, index+1);
 		return count;
 	}
 }
