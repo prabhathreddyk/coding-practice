e0a5a8bae6490396956f58d2672c243d3fc34f4d
prabhathkatta <prabhathreddy29@gmail.com>
2020-05-31 12:36:14 -0500

    Binary Tree

--- src/main/java/edu/code/process/BinaryTreeImplementation.java
+++ src/main/java/edu/code/process/BinaryTreeImplementation.java
@@ -8,7 +8,7 @@ import edu.code.process.utility.CollectionUtils;
 public class BinaryTreeImplementation {
 
 	public static void main(String[] args) {
-		/*BinaryTreeImplementation bti = new BinaryTreeImplementation();
+		BinaryTreeImplementation bti = new BinaryTreeImplementation();
 		BinaryTree binaryTree = getBinaryTree();
 		BinaryTree balancedBinaryTree = getBalancedBinaryTree();
 
@@ -28,7 +28,7 @@ public class BinaryTreeImplementation {
 		
 		// Recursive
 		System.out.println(" PreOrder Recursive: "+ preOrderTraversal(balancedBinaryTree));
-		System.out.println(" PostOrder Recursive: "+ postOrder(balancedBinaryTree));*/
+		System.out.println(" PostOrder Recursive: "+ postOrder(balancedBinaryTree));
 		
 		//Build Binary tree from Inorder and postOrder
 		int[] inorder = {9,3,15,20,7};
